da75833608e2b79187d1b8ce240d96fd37fcab95
Douglas de Paiva <douglaspaivadomingues@gmail.com>
2024-03-04 18:39:39 -0300

    'req 03 - novo'

--- src/loginRoutes.js
+++ src/loginRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
-const router = express.Router();
 const talkerDB = require('./talkerDB');
+
+const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { email, password } = req.body;
