09830762bf58833567e8f2c0047f9f479bdb1a46
Douglas de Paiva <douglaspaivadomingues@gmail.com>
2024-03-05 15:38:20 -0300

    'req 05'

--- src/talkerDB.js
+++ src/talkerDB.js
@@ -2,6 +2,8 @@ const fs = require('fs').promises;
 const path = require('path');
 const getToken = require('./utils');
 
+let token = '';
+
 const getAll = async () => {
   const data = await fs.readFile(path.resolve('./src/talker.json'), 'utf8');
   const talkers = JSON.parse(data);
@@ -14,16 +16,29 @@ const getById = async (id) => {
   return talkers.find((talker) => talker.id === id);
 };
 
-const getPostWithToken = async (email, password) => {
-  let token = '';
+const getPostWithToken = async (email, password) => { 
   if (email && password) {
     token = getToken();
   }
   return token;
 };
 
+const addTalker = async (requestBody) => {
+  const data = await fs.readFile(path.resolve('./src/talker.json'), 'utf8');
+  const talkers = JSON.parse(data);
+  const newTalker = {
+    id: talkers.length + 1,
+    ...requestBody,
+  };
+  talkers.push(newTalker);
+  await fs.writeFile(path.resolve('./src/talker.json'), JSON.stringify(talkers));
+  return newTalker;
+};
+
 module.exports = {
   getAll,
   getById,
   getPostWithToken,
+  token,
+  addTalker,
 };
--- src/talkerRoutes.js
+++ src/talkerRoutes.js
@@ -4,6 +4,90 @@ const talkerDB = require('./talkerDB');
 
 const router = express.Router();
 
+const DATA_REGEX = /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  } 
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  if (age === undefined || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (!Number.isInteger(age) || age < 18) {
+    return res.status(400)
+      .json({ message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' });
+  }
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk && typeof talk !== 'object') {
+    return res.status(400)
+      .json({ message: 'O campo "talk" é obrigatório' });
+  }
+  next();
+};
+
+const validateWatchedAt = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk.watchedAt) {
+    return res.status(400)
+      .json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!talk.watchedAt || talk.watchedAt === '') {
+    return res.status(400)
+      .json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  if (!DATA_REGEX.test(talk.watchedAt)) {
+    return res.status(400)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const validateTalkRate = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk.rate && talk.rate !== 0) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  }
+
+  next();
+};
+
+const validateTalkwithRate = (req, res, next) => {
+  const { talk } = req.body;
+  if (!Number.isInteger(talk.rate) || talk.rate === 0) {
+    return res.status(400)
+      .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+  }
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(400)
+      .json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+  }
+  next();
+};
+
+const validateToken = (req, res, next) => {
+  const autToken = req.headers.authorization;
+  if (!autToken) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (autToken.length !== 16 || typeof autToken !== 'string') {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
 router.get('/', async (req, res) => {
   try {
     const result = await talkerDB.getAll();
@@ -27,5 +111,16 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+router.post('/', validateName, validateToken, validateAge, validateTalk,
+  validateWatchedAt, validateTalkRate, validateTalkwithRate, async (req, res) => {
+    try {
+      const result = await talkerDB.addTalker(req.body);
+      res.status(201).json(result);
+    } catch (error) {
+      console.error('Erro na solicitação:', error);
+      res.status(500).json({ message: 'Erro na solicitação' });
+    }
+  });
 
 module.exports = router;
